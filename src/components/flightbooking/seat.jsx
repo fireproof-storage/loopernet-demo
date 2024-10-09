e5b821d2606514170b9c4296de247e3e804c3b92
Chris Anderson <jchris@gmail.com>
2024-10-08 20:35:45 -0700

    current?

--- src/components/flightbooking/seat.jsx
+++ src/components/flightbooking/seat.jsx
@@ -12,10 +12,14 @@ class Seat extends React.Component {
   render() {
     const { seat, available, currentSeat, forwardedRef } = this.props;
     const isSelected = currentSeat === seat;
+      
+    const isCurrent = currentSeat === seat
+    const availableButtonClasses = isCurrent ? `${SeatStyle.button} ${SeatStyle.current}` : `${SeatStyle.button}`
     return (
       <div
         ref={forwardedRef} // Attach the ref here
-        className={`${SeatStyle.button} ${!available ? SeatStyle.button_unavailable : ''} ${isSelected ? SeatStyle.current : ''}`}
+        // className={`${SeatStyle.button} ${!available ? SeatStyle.button_unavailable : ''} ${isSelected ? SeatStyle.current : ''}`}
+        className={!available ? `${SeatStyle.button} ${SeatStyle.button_unavailable}` : isSelected ? `${SeatStyle.button} ${SeatStyle.current}` : `${SeatStyle.button}`}
         onClick={this.handleClick}
       >
         {/* {seat} */}
