84ff29c0ca6af73bd2f05be84ab75801ab8fb5d5
Chris Anderson <jchris@gmail.com>
2024-10-08 20:59:24 -0700

    selected yea

--- src/components/flightbooking/seat.jsx
+++ src/components/flightbooking/seat.jsx
@@ -11,16 +11,23 @@ class Seat extends React.Component {
 
   render() {
     const { seat, available, currentSeat, forwardedRef } = this.props;
-    const isSelected = currentSeat === seat;
-      
-    const isCurrent = currentSeat === seat
-    const availableButtonClasses = isCurrent ? `${SeatStyle.button} ${SeatStyle.current}` : `${SeatStyle.button}`
-    return (
-      <div
+    const isCurrent = currentSeat === seat;
+    const availableButtonClasses = isCurrent
+      ? `${SeatStyle.button} ${SeatStyle.current}`
+      : `${SeatStyle.button}`;
+
+    return available ? (
+      <button
         ref={forwardedRef} // Attach the ref here
-        // className={`${SeatStyle.button} ${!available ? SeatStyle.button_unavailable : ''} ${isSelected ? SeatStyle.current : ''}`}
-        className={!available ? `${SeatStyle.button} ${SeatStyle.button_unavailable}` : isSelected ? `${SeatStyle.button} ${SeatStyle.current}` : `${SeatStyle.button}`}
+        className={availableButtonClasses}
         onClick={this.handleClick}
+      >
+        {/* {seat} */}
+      </button>
+    ) : (
+      <div
+        title={`Seat ${seat} is unavailable`}
+        className={`${SeatStyle.button} ${SeatStyle.button_unavailable}`}
       >
         {/* {seat} */}
       </div>
